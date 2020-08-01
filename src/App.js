a51765108f3b107b7cca326eeb38ae81c4f2d7ca
SuperflyEks <paul.okpara1996@gmail.com>
2020-08-01 11:31:33 +0100

    added sign up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 class App extends React.Component{
@@ -16,13 +16,23 @@ class App extends React.Component{
     }
   }
 
-  unSubscribeFromAuth = null
+  unSubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }
+      else { this.setState( {currentUser: userAuth} );}
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,41 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCnb9CyFA8Cn2eMm_0XFQLEt2r7nEoCcbk",
-    authDomain: "royalty-db-b40bf.firebaseapp.com",
-    databaseURL: "https://royalty-db-b40bf.firebaseio.com",
-    projectId: "royalty-db-b40bf",
-    storageBucket: "royalty-db-b40bf.appspot.com",
-    messagingSenderId: "205508565397",
-    appId: "1:205508565397:web:8892445e9370fa50477e9c"
+    apiKey: "AIzaSyBca2Ra4GJU5waza6WVZeVGOZqD71ZjNJU",
+    authDomain: "royalty-clothings-db.firebaseapp.com",
+    databaseURL: "https://royalty-clothings-db.firebaseio.com",
+    projectId: "royalty-clothings-db",
+    storageBucket: "royalty-clothings-db.appspot.com",
+    messagingSenderId: "568259334496",
+    appId: "1:568259334496:web:660b08207ab62f27e88e05"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+}
+
 
 firebase.initializeApp(config);
 
@@ -20,5 +47,4 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ promt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
 export default firebase;
