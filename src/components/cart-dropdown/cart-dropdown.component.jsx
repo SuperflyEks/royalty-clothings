85269f5ae322eae55c440bd4c9c2765b7603cd0c
SuperflyEks <paul.okpara1996@gmail.com>
2020-08-09 15:32:56 +0100

    Updated shopdata with new data, updated cart utils to group items in cart reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
-import CartIcon from '../cart-icon/cart-icon.component';
 
 const CartDropdown = () => (
     <div className='cart-dropdown'>
